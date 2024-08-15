c3e3da2fe6c64dadd1d96aacf00744f9eecb3d9e
Livingstone Okech <oklivinstone@gmail.com>
2024-08-15 21:18:00 +0000

    updates

--- 0x00-python_variable_annotations/7-to_kv.py8-make_multiplier.py
+++ 0x00-python_variable_annotations/7-to_kv.py8-make_multiplier.py
@@ -1,15 +1,7 @@
 #!/usr/bin/env python3
 """ Complex types - string and int/float to tuple """
-from typing import List, Union, Tuple
-
+from typing import Union, Tuple
 
 def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
-    """ Type-annotated function to_kv that takes
-        strings, integers and floats arguments.
-        Args:
-            k: string type.
-            v: int or float type.
-        Return:
-            Tuple.
-    """
-    return (k, pow(v, 2))
+    """Return a tuple where the first element is the string and the second is the square of the int/float."""
+    return (k, float(v ** 2))
